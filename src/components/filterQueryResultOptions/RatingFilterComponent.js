255621e333545986d586855cecd841cfe647d508
David Mullally <mullallydavid7@gmail.com>
2023-02-07 23:29:29 +0100

    imporve component for category filters

--- src/components/filterQueryResultOptions/RatingFilterComponent.js
+++ src/components/filterQueryResultOptions/RatingFilterComponent.js
@@ -5,20 +5,19 @@ import { Rating } from "react-simple-star-rating";
 const RatingFilterComponent = () => {
   return (
     <>
+      <span className="fw-bold">Rating</span>
       {Array.from({ length: 5 }).map((_, index) => {
         return (
-          <Fragment key={index}>
-            <Form.Check type="checkbox" id={`check-api-${index}`}>
-              <Form.Check.Input type="checkbox" isValid />
-              <Form.Check.Label style={{ cursor: "pointer" }}>
-                <Rating readonly size={20} initialValue={5 - index} />
-              </Form.Check.Label>
-            </Form.Check>
-          </Fragment>
+            <Fragment key={index}>
+              <Form.Check type="checkbox" id={`check-api-${index}`}>
+                <Form.Check.Input type="checkbox" isValid />
+                <Form.Check.Label style={{ cursor: "pointer" }}>
+                  <Rating readonly size={20} initialValue={5 - index} />
+                </Form.Check.Label>
+              </Form.Check>
+            </Fragment>
         );
       })}
-
-      <span className="fw-bold">Rating</span>
     </>
   );
 };
