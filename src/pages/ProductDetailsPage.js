a9a02e0dbc7a399c2bdc74007111df0f062f7cd8
David Mullally <mullallydavid7@gmail.com>
2023-02-08 18:06:20 +0100

    structure the prioduct details page

--- src/pages/ProductDetailsPage.js
+++ src/pages/ProductDetailsPage.js
@@ -1,9 +1,28 @@
+import { Col, Container, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
 const ProductDetailsPage = () => {
-    const { productId } = useParams();
-    console.log(productId)
-    return <p>This is the product details page</p>
-}
+  const { productId } = useParams();
+  return (
+    <Container>
+      <Row className="mt-5">
+        <Col md={4}>images</Col>
+        <Col md={8}>
+          <Row>
+            <Col md={8}>product name, price, description, rating</Col>
+            <Col md={4}>product status, quantity</Col>
+          </Row>
+          <Row>
+            <Col className="mt-5">
+              <h5>REVIEWS</h5>
+            </Col>
+          </Row>
+          <hr />
+          send review form
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default ProductDetailsPage;
