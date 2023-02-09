b0029a889e76677d5a52a5faf330514082e0725d
David Mullally <mullallydavid7@gmail.com>
2023-02-09 23:41:16 +0100

    create products page for the admin

--- src/pages/admin/AdminProductsPage.js
+++ src/pages/admin/AdminProductsPage.js
@@ -1,5 +1,73 @@
+import { Button, Col, Row, Table } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import AdminLinksComponent from "../../components/admin/AdminLinksComponent";
+const deleteHandler = () => {
+  if (window.confirm("Are you sure?")) alert("product deleted successfully");
+};
 const AdminProductsPage = () => {
-    return <p>This is the admin  products page</p>
-}
+  return (
+    <Row className="m-5">
+      <Col md={2}>
+        <AdminLinksComponent />
+      </Col>
+      <Col md={10}>
+        <h1>
+          Product List{" "}
+          <LinkContainer to="/admin/create-product">
+            <Button variant="primary" size="lg">
+              Create New
+            </Button>
+          </LinkContainer>
+        </h1>
+        <Table striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Product Name</th>
+              <th>Price</th>
+              <th>Category</th>
+              <th>Edit/Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {[
+              { name: "Sony-WZH", price: 1000, category: "TVs" },
+              { name: "Panasonic-UI7", price: 1250, category: "Monitors" },
+              {
+                name: "The Last Of Us Part II",
+                price: 1140,
+                category: "Games",
+              },
+            ].map((item, idx) => {
+              return (
+                <tr key={idx}>
+                  <td>{idx + 1}</td>
+                  <td>{item.name}</td>
+                  <td>{item.price}</td>
+                  <td>{item.category}</td>
+                  <td>
+                    <LinkContainer to="/admin/edit-product">
+                      <Button className="btn-sm">
+                        <i className="bi bi-pencil-square"></i>
+                      </Button>
+                    </LinkContainer>
+                    {" / "}
+                    <Button
+                      variant="danger"
+                      className="btn-sm"
+                      onClick={deleteHandler}
+                    >
+                      <i className="bi bi-x-circle"></i>
+                    </Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
+  );
+};
 
 export default AdminProductsPage;
