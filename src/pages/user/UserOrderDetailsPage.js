92383e002fde6f73cae2dccbf6e4aad128ab86d2
David Mullally <mullallydavid7@gmail.com>
2023-02-09 16:53:53 +0100

    structure user order details page

--- src/pages/user/UserOrderDetailsPage.js
+++ src/pages/user/UserOrderDetailsPage.js
@@ -1,5 +1,27 @@
+import { Col, Container, Row } from "react-bootstrap";
+
 const UserOrderDetailsPage = () => {
-    return <p>This is the user order details page</p>
-}
+  return (
+    <Container fluid>
+      <Row className="mt-4">
+        <h1>Order Details</h1>
+        <Col md={8}>
+          <br />
+          <Row>
+            <Col md={6}>
+              <h2>Shipping</h2>
+            </Col>
+            <Col md={6}>
+              <h2>Payment Method</h2>
+            </Col>
+          </Row>
+        </Col>
+        <Col md={4}>
+          <h3>Order Summary</h3>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default UserOrderDetailsPage;
