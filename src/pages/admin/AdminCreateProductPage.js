5609e64ea2457c3b4d49fec84d81344d7c4ef265
David Mullally <mullallydavid7@gmail.com>
2023-02-10 14:10:51 +0100

    finish the page for creating products

--- src/pages/admin/AdminCreateProductPage.js
+++ src/pages/admin/AdminCreateProductPage.js
@@ -1,11 +1,21 @@
 import { useState } from "react";
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import {
+  Alert,
+  Button,
+  CloseButton,
+  Col,
+  Container,
+  Form,
+  Row,
+  Table,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const AdminCreateProductPage = () => {
   const [validated, setValidated] = useState(false);
 
-  {/*const onChange = () => {
+  {
+    /*const onChange = () => {
     const password = document.querySelector("input[name=password]");
     const passwordConfirm = document.querySelector(
       "input[name=passwordConfirm]"
@@ -15,7 +25,8 @@ const AdminCreateProductPage = () => {
     } else {
       passwordConfirm.setCustomValidity("Passwords don't match");
     }
-  };*/}
+  };*/
+  }
 
   const handleSubmit = (event) => {
     const form = event.currentTarget;
@@ -35,7 +46,7 @@ const AdminCreateProductPage = () => {
           </Link>
         </Col>
         <Col md={6}>
-          <h1>Create A New Product</h1>
+          <h3>Create A New Product</h3>
           <Form noValidate validated={validated} onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicProductName">
               <Form.Label>Product Name</Form.Label>
@@ -54,7 +65,7 @@ const AdminCreateProductPage = () => {
               name="productDescription"
               required
               as="textarea"
-              rows={3}
+              rows={2}
             />
           </Form.Group>
           <Form.Group
@@ -69,7 +80,10 @@ const AdminCreateProductPage = () => {
             <Form.Control name="productPrice" required type="number" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="form.ProductCategoryTextArea">
-            <Form.Label>Category</Form.Label>
+            <Form.Label>
+              Category
+              <CloseButton />(<small>remove select</small>)
+            </Form.Label>
             <Form.Select
               required
               name="producCategory"
@@ -81,11 +95,83 @@ const AdminCreateProductPage = () => {
               <option value="3">Games</option>
             </Form.Select>
           </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicNewCategory">
+            <Form.Label>
+              Or create a new category (e.g. Computers, Laptops etc.){""}
+            </Form.Label>
+            <Form.Control name="newProductCategory" type="text" />
+          </Form.Group>
+
+          <Row className="mt-5">
+            <Col md={6}>
+              <Form.Group className="mb-3" controlId="formBasicAttributes">
+                <Form.Label>Choose Attribute and set value</Form.Label>
+                <Form.Select name="atrrKey" aria-label="Default select example">
+                  <option>Choose Attribute</option>
+                  <option value="Color">Color</option>
+                </Form.Select>
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+            <Form.Group className="mb-3" controlId="formBasicAttributeValue">
+                <Form.Label>Attribute value</Form.Label>
+                <Form.Select name="atrrValue" aria-label="Default select example">
+                  <option>Choose Attribute Value</option>
+                  <option value="red">Red</option>
+                </Form.Select>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Table hover>
+              <thead>
+                <tr>
+                  <th>Attribute</th>
+                  <th>Value</th>
+                  <th>Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                  <td>attr key</td>
+                  <td>attr value</td>
+                  <td><CloseButton /></td>
+                  </tr>
+                </tbody>
+            </Table>
+            <Row>
+              <Col md={6}>
+                <Form.Group className="mb-3" controlId="formBasicNewProductAttribute">
+                  <Form.Label>Create new product attribute</Form.Label>
+                  <Form.Control disabled={false}
+                  placeholder="first choose or create a category"
+                  name="newProductAttrValue"
+                  type="text" />
+                </Form.Group>
+               
+              </Col>
+              <Col md={6}>
+              <Form.Group className="mb-3" controlId="formBasicNewProductAttributeValue">
+                  <Form.Label>Create new product attribute value</Form.Label>
+                  <Form.Control disabled={false}
+                  placeholder="first choose or create a category"
+                  name="newProductAttrValueValue"
+                  type="text" />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Alert variant="primary">After typing the attributes key and value, press enter on one of these fields </Alert>
+          </Row>
           <Form.Group controlId="formMultiple" className="mb-3 mt-3">
             <Form.Label>Images</Form.Label>
             <Form.Control required type="file" multiple />
           </Form.Group>
-          <Button variant="primary" type="submit" className="mb-5">
+          <Button
+            variant="primary"
+            type="submit"
+            style={{ marginBottom: "12vh" }}
+          >
             Create
           </Button>
         </Col>
