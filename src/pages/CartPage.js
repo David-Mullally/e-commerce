82b3390a9ca0fbada7b27b3fd493f0477cafaf36
Dave Mullally <mullallydavid7@gmail.com>
2023-05-10 13:04:31 +0200

    split cartPage into page and component and start developing it

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,37 +1,19 @@
-import { Alert, Button, Col, Container, ListGroup, Row } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
-import CartItemComponent from "../components/CartItemComponent";
+import CartPageComponent from "./components/CartPageComponent";
+
+import { useSelector, useDispatch } from "react-redux";
+import { addToCart } from "../redux/actions/cartActions";
 
 const CartPage = () => {
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  const cartSubtotal = useSelector((state) => state.cart.cartSubtotal);
+  const reduxDispatch = useDispatch();
   return (
-    <Container>
-      <Row className="mt-4">
-        <Col md={8}>
-                  <h1>Shopping Cart</h1>
-                  <ListGroup variant="flush">
-          {Array.from({ length: 3 }).map((item, idx) => (
-            <CartItemComponent item={{ image: { path: "/images/tablets-category.png" }, name: "Product name", price: 10, count: 10, quantity: 10 }} key={idx} />
-          ))}
-          </ListGroup>
-          <Alert varient="info">Your Cart is empty</Alert>
-        </Col>
-        <Col md={4}>
-          <ListGroup>
-            <ListGroup.Item>
-              <h3>Subtotal (2)</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              Price: <span className="fw-bold">$800</span>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <LinkContainer to="/user/cart-details">
-                <Button type="button">Proceed To Checkout</Button>
-              </LinkContainer>
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-      </Row>
-    </Container>
+    <CartPageComponent
+      addToCart={addToCart}
+      cartItems={cartItems}
+      cartSubtotal={cartSubtotal}
+      reduxDispatch={reduxDispatch}
+    />
   );
 };
 
