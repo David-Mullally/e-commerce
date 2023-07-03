6209f5a2af558d98090f4dbee37381b0b1843149
Dave Mullally <mullallydavid7@gmail.com>
2023-07-03 18:54:55 +0200

    Display Text Written by Admin in the Admin Chatbox

--- src/components/admin/AdminChatRoomComponent.js
+++ src/components/admin/AdminChatRoomComponent.js
@@ -1,41 +1,81 @@
 import { Fragment, useState } from "react";
 import { Button, Form, Toast } from "react-bootstrap";
 
-const AdminChatRoomComponent = ({chatRoom, roomIndex, socketUser}) => {
-  [window["toast" + roomIndex], window["closeToast" + roomIndex]] = useState(true);
+const AdminChatRoomComponent = ({ chatRoom, roomIndex, socketUser }) => {
+  [window["toast" + roomIndex], window["closeToast" + roomIndex]] =
+    useState(true);
 
   const close = () => {
     window["closeToast" + roomIndex](false);
-  }
+  };
+
+  const [rerender, setRerender] = useState(false);
+
+  const adminSubmitChatMsg = (e, elm) => {
+    e.preventDefault();
+    if (e.keyCode && e.keyCode !== 13) {
+      return;
+    }
+    const msg = document.getElementById(elm);
+    let v = msg.value.trim();
+    console.log("adminMsg", v)
+    if (v === "" || v === null || v === false || !v) {
+      return;
+    }
+    chatRoom[1].push({ admin: msg.value });
+    setRerender(!rerender);
+  };
 
   return (
-      <div style={{ display: "flex"}}>
-      <Toast show={window["toast" + roomIndex]} onClose={()=> close()} className="ms-4 mb-5">
+    <div style={{ display: "flex" }}>
+      <Toast
+        show={window["toast" + roomIndex]}
+        onClose={() => close()}
+        className="ms-4 mb-5"
+      >
         <Toast.Header>
           <strong className="me-auto">Chat With User</strong>
         </Toast.Header>
         <Toast.Body>
-          <div style={{ maxHeight: "500px", overflow: "auto" }}>
+          <div
+            className={`cht-msg${socketUser}`}
+            style={{ maxHeight: "500px", overflow: "auto" }}
+          >
             {chatRoom[1].map((msg, idx) => (
               <Fragment key={idx}>
                 {msg.client && (
-                  <p key={idx} className="bg-primary p-3 ms-4 text-light rounded-pill">
-                  <b>User Wrote:</b> {msg.client}
-                </p>
+                  <p
+                    key={idx}
+                    className="bg-primary p-3 ms-4 text-light rounded-pill"
+                  >
+                    <b>User Wrote:</b> {msg.client}
+                  </p>
                 )}
                 {msg.admin && (
                   <p key={idx}>
-                  <b>You Wrote:</b> {msg.admin}
-                </p>
+                    <b>You Wrote:</b> {msg.admin}
+                  </p>
                 )}
               </Fragment>
             ))}
           </div>
-          <Form.Group>
+          <Form.Group controlId={`adminChatMsg${roomIndex}`}>
             <Form.Label>Write A Message</Form.Label>
-            <Form.Control as="textarea" rows={2}></Form.Control>
+            <Form.Control
+              onKeyUp={(e) =>
+                adminSubmitChatMsg(e, `adminChatMsg${roomIndex}`)
+              }
+              as="textarea"
+              rows={2}
+            ></Form.Control>
           </Form.Group>
-          <Button variant="success" type="submit">
+          <Button
+            onClick={(e) =>
+              adminSubmitChatMsg(e, `adminChatMsg${roomIndex}`)
+            }
+            variant="success"
+            type="submit"
+          >
             Submit
           </Button>
         </Toast.Body>
